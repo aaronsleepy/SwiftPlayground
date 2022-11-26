79c94efa1daa3c86b32816508f2e9395c6300167
aaronsleepy <aaron@kmong.com>
2022-11-26 12:27:12 +0900

    fix: 코드 파일 이동

--- Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -3,18 +3,3 @@
 import Foundation
 import Combine
 
-let subject = PassthroughSubject<String, Never>()
-
-
-let subscription = subject
-    .print("[Debug]")
-    .sink { value in
-    print(">>> Subscriber received value: \(value)")
-}
-
-subject.send("Hello")
-subject.send("Hello again!")
-subject.send("Hello for the last time!")
-subject.send(completion: .finished) // <-- Publisher가 종료 이벤트 전송
-//subscription.cancel() // Subscriber가 종료 요청
-subject.send("Hello ?? :(")
