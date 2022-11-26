9c408fa0edda99fc8dbe5ea86fc2aa8f99404402
aaronsleepy <aaron@kmong.com>
2022-11-26 12:07:45 +0900

    feat: assign subscriber 연습

--- Hello Combine.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
@@ -14,3 +14,16 @@ let arrayPublisher = [1, 3, 5, 7, 9].publisher
 let subscription2 = arrayPublisher.sink { value in
     print("Received Value: \(value)")
 }
+
+// assign
+class MyClass {
+    var property: Int = 0 {
+        didSet {
+            print("Did set property to \(property)")
+        }
+    }
+}
+
+let object = MyClass()
+let subscriptio = arrayPublisher.assign(to: \.property, on: object)
+print("Final value: \(object.property)")
