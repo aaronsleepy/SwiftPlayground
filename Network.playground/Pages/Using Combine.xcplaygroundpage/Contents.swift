044328f68b200849973ee3dfe24a1cf2d85c9a5e
aaronsleepy <aaron@kmong.com>
2022-11-26 22:43:14 +0900

    feat: NetworkService를 dataTaskPublisher 형태로 전환

--- Network.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
+++ Network.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -36,34 +36,23 @@ final class NetworkService {
         session = URLSession(configuration: configuration)
     }
     
-    func fetchProfile(userName: String, completion: @escaping (Result<GithubProfile, Error>) -> Void) {
+    func fetchProfile(userName: String) -> AnyPublisher<GithubProfile, Error> {
         let url = URL(string: "https://api.github.com/users/\(userName)")!
-
-        let task = session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(NetworkError.transportError(error)))
-                return
-            }
-            
-            if let httpReponse = response as? HTTPURLResponse, !(200..<300).contains(httpReponse.statusCode) {
-                completion(.failure(NetworkError.responseError(statusCode: httpReponse.statusCode)))
-                return
-            }
-            
-            guard let data = data else {
-                completion(.failure(NetworkError.noData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let profile = try decoder.decode(GithubProfile.self, from: data)
-                completion(.success(profile))
-            } catch let error as NSError {
-                completion(.failure(NetworkError.decodingError(error)))
-            }
-        }
         
-        task.resume()
+        let publisher = session.dataTaskPublisher(for: url)
+            // 서버에서 받은 response 확인
+            .tryMap { result -> Data in
+                guard let httpReponse = result.response as? HTTPURLResponse, (200..<300).contains(httpReponse.statusCode) else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                return result.data
+            }
+            // 받은 Data 디코딩
+            .decode(type: GithubProfile.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        
+        return publisher
     }
 }
