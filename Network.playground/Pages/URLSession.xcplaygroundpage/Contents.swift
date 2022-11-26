602a8265f7edda6a18d9dd46015eec4a4c554563
aaronsleepy <aaron@kmong.com>
2022-11-26 22:08:28 +0900

    feat: dataTask 기본 활용 추가

--- Network.playground/Pages/URLSession.xcplaygroundpage/Contents.swift
+++ Network.playground/Pages/URLSession.xcplaygroundpage/Contents.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+let configuration = URLSessionConfiguration.default
+let session = URLSession(configuration: configuration)
+
+let url = URL(string: "https://api.github.com/users/aaronsleepy")!
+
+let task = session.dataTask(with: url) { data, response, error in
+    guard let httpReponse = response as? HTTPURLResponse, (200..<300).contains(httpReponse.statusCode) else {
+        print(">>> response \(response)")
+        return
+    }
+    
+    guard let data = data else { return }
+    
+    let result = String(data: data, encoding: .utf8)
+    print(result)
+}
+
+task.resume()
