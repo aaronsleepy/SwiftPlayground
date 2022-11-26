185ade0af4e25f8e9dde6b3535549afa0c7edabf
aaronsleepy <aaron@kmong.com>
2022-11-26 12:18:30 +0900

    feat PassthroughSubject & CurrentValueSubject 연습

--- Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
@@ -2,3 +2,39 @@
 
 import Foundation
 import Combine
+
+/**
+ PassthroughSubject
+ */
+let relay = PassthroughSubject<String, Never>()
+relay.send("Initial text")
+let subscription1 = relay.sink { value in
+    print("subscription1 received value: \(value)")
+}
+
+relay.send("Hello")
+relay.send("World")
+relay.values
+
+
+/**
+ CurrentValueSubject
+ */
+let variable = CurrentValueSubject<String, Never>("")
+variable.send("Initial text")
+
+let subscription2 = variable.sink { value in
+    print("subscription2 received value: \(value)")
+}
+
+variable.send("More text")
+variable.send("And more text")
+
+variable.value
+
+
+
+// 이건?
+let publisher = ["Hello", "we", "go"].publisher
+publisher.subscribe(relay)
+publisher.subscribe(variable)
