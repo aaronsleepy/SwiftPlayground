7086116f68ffa8c10536bca2b84d092f5a4329e7
aaronsleepy <aaron@kmong.com>
2022-11-26 20:30:40 +0900

    feat: Operator 추가 예제 - removeDuplicates() - compactMap - ignoreOutput

--- Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -3,4 +3,40 @@
 import Foundation
 import Combine
 
+var subscriptions = Set<AnyCancellable>()
 
+/**
+ removeDuplicates
+ */
+let words = "hey hey there! Mr MR ?"
+    .components(separatedBy: " ")
+    .publisher
+
+words
+    .removeDuplicates()
+    .sink { value in
+        print(">>> \(value)")
+    }.store(in: &subscriptions)
+
+/**
+ compactMap
+ */
+let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
+strings
+    .compactMap { Float($0) }
+    .sink { value in
+        print(">>> \(value)")
+    }.store(in: &subscriptions)
+
+/**
+ ignoreOutput
+ */
+let numbers = (1...10_000).publisher
+numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: { print("Completed with: \($0)")}, receiveValue: { print($0) })
+    .store(in: &subscriptions)
+
+/**
+ prefix
+ */
