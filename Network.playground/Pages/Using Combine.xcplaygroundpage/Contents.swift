9950c9ebfa2f8f7f905877372763452b49b159c2
aaronsleepy <aaron@kmong.com>
2022-11-26 22:34:54 +0900

    feat: NetworkService 복붙

--- Network.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
+++ Network.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -24,3 +24,46 @@ struct GithubProfile: Codable {
         case following
     }
 }
+
+
+/**
+ 네트워크를 담당할 Service: Combine 버전으로 전환
+ */
+final class NetworkService {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
+    
+    func fetchProfile(userName: String, completion: @escaping (Result<GithubProfile, Error>) -> Void) {
+        let url = URL(string: "https://api.github.com/users/\(userName)")!
+
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(NetworkError.transportError(error)))
+                return
+            }
+            
+            if let httpReponse = response as? HTTPURLResponse, !(200..<300).contains(httpReponse.statusCode) {
+                completion(.failure(NetworkError.responseError(statusCode: httpReponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let profile = try decoder.decode(GithubProfile.self, from: data)
+                completion(.success(profile))
+            } catch let error as NSError {
+                completion(.failure(NetworkError.decodingError(error)))
+            }
+        }
+        
+        task.resume()
+    }
+}
