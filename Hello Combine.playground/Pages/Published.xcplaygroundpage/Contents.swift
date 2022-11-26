26b4e706da35c459cdaffd48ec87634048796c7d
aaronsleepy <aaron@kmong.com>
2022-11-26 12:44:06 +0900

    feat: @Publshed를 이용하여 ViewModel의 데이터를 View Object에 assign

--- Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -3,3 +3,22 @@
 import Foundation
 import Combine
 
+final class SomeViewModel {
+    @Published var name: String = "Jack"
+    var age: Int = 20
+}
+
+final class Label {
+    var text: String = "Empty"
+}
+
+let label = Label()
+let viewModel = SomeViewModel()
+
+print(">>> text: \(label.text)")
+    
+viewModel.$name.assign(to: \.text, on: label)
+print(">>> text: \(label.text)")
+
+viewModel.name = "Aaron"
+print(">>> text: \(label.text)")
