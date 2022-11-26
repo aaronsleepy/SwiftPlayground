8e432d4838082c8f2d1ff76ac785b6625a6064fa
aaronsleepy <aaron@kmong.com>
2022-11-26 20:07:33 +0900

    feat: combineLatest 기본

--- Hello Combine.playground/Pages/Operator - combineLatest.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - combineLatest.xcplaygroundpage/Contents.swift
@@ -3,4 +3,35 @@
 import Foundation
 import Combine
 
+/**
+ 2개의 Publisher로부터 동시에 받고 싶을 때
+ */
+
+// Basic CombineLatest
+let strPublisher = PassthroughSubject<String, Never>()
+let numPublisher = PassthroughSubject<Int, Never>()
+
+// |--> time
+// 1: "a",            "b",    "c"
+// 2:     1      2     3            4
+// r:    a:1    a:2   b:3     c:3  c:4
+
+strPublisher.combineLatest(numPublisher).sink { (str, num) in
+    print("Receive: \(str):\(num)")
+}
+
+strPublisher.send("a")
+numPublisher.send(1)
+numPublisher.send(2)
+strPublisher.send("b")
+numPublisher.send(3)
+strPublisher.send("c")
+numPublisher.send(4)
+
+
+// Advanced CombineLatest
+
+
+// Merge
+
 
