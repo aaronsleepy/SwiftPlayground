656f521975bbfd302213ce054a3e9a47dc88c0ee
aaronsleepy <aaron@kmong.com>
2022-11-26 22:12:53 +0900

    feat: Network 기본기능 복붙 from URLSession Playground

--- Network.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
+++ Network.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
@@ -15,3 +15,22 @@ struct GithubProfile: Codable {
         case following
     }
 }
+
+let configuration = URLSessionConfiguration.default
+let session = URLSession(configuration: configuration)
+
+let url = URL(string: "https://api.github.com/users/aaronsleepy")!
+
+let task = session.dataTask(with: url) { data, response, error in
+    guard let httpReponse = response as? HTTPURLResponse, (200..<300).contains(httpReponse.statusCode) else {
+        print(">>> response \(response)")
+        return
+    }
+    
+    guard let data = data else { return }
+    
+    let result = String(data: data, encoding: .utf8)
+    print(result)
+}
+
+task.resume()
