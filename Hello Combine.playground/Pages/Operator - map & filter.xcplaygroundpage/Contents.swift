971b659319d17d876aba6f2a1fb9d7bf92217d88
aaronsleepy <aaron@kmong.com>
2022-11-26 13:13:32 +0900

    feat: Operator 연습 - map, filter

--- Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -3,4 +3,33 @@
 import Foundation
 import Combine
 
+/**
+ Transform - Map
+ */
+let numPublisher = PassthroughSubject<Int, Never>()
+let subscription = numPublisher
+    .map { $0 * 2 }
+    .sink { value in
+        print(">>> Transformed Value: \(value)")
+    }
 
+numPublisher.send(10)
+numPublisher.send(20)
+numPublisher.send(30)
+subscription.cancel()
+
+/**
+ Fllter
+ */
+let stringPublisher = PassthroughSubject<String, Never>()
+let subscription2 = stringPublisher
+    .filter { $0.contains("a") }
+    .sink { value in
+        print(">>> Filtered Value: \(value)")
+    }
+
+stringPublisher.send("aaron")
+stringPublisher.send("jack")
+stringPublisher.send("jenny")
+stringPublisher.send("john")
+subscription2.cancel()
