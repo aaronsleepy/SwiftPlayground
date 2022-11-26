fda5a1ba1f0022c11e5a4cbdc1a50a541edf0846
aaronsleepy <aaron@kmong.com>
2022-11-26 22:24:15 +0900

    feat: 깃헙 프로필 dataTask 코드 복붙

--- Network.playground/Pages/Fetch Method.xcplaygroundpage/Contents.swift
+++ Network.playground/Pages/Fetch Method.xcplaygroundpage/Contents.swift
@@ -32,6 +32,23 @@ final class NetworkService {
     }
     
     func fetchProfile(userName: String, completion: @escaping (Result<GithubProfile, Error>) -> Void) {
-        
+        let url = URL(string: "https://api.github.com/users/aaronsleepy")!
+
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let httpReponse = response as? HTTPURLResponse, (200..<300).contains(httpReponse.statusCode) else {
+                print(">>> response \(response)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let profile = try decoder.decode(GithubProfile.self, from: data)
+                print("profile: \(profile)")
+            } catch let error as NSError {
+                print("error: \(error)")
+            }
+        }
     }
 }
