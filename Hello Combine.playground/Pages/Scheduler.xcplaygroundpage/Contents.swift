66927800578f03b70018cc90b6ae96576d7b415a
aaronsleepy <aaron@kmong.com>
2022-11-26 13:07:46 +0900

    Scheduler 사용 연습

--- Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -4,3 +4,28 @@ import Foundation
 import Combine
 
 
+let arrayPublisher = [1, 2, 3].publisher
+
+let queue = DispatchQueue(label: "customQueue")
+
+let subscription = arrayPublisher
+    .map { value -> Int in
+        print(">>> transform: \(value), thread: \(Thread.current)")
+        return value
+    }
+    .sink { value in
+    print(">>> Received Value: \(value), thread: \(Thread.current)")
+}
+
+
+// map 태스크가 heavy하다면, 별도의 Thread에서 실행
+let subscription2 = arrayPublisher
+    .subscribe(on: queue)
+    .map { value -> Int in
+        print(">>> transform: \(value), thread: \(Thread.current)")
+        return value
+    }
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+    print(">>> Received Value: \(value), thread: \(Thread.current)")
+}
