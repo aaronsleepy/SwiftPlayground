d56ae54c10b27c6d6d4ac17c4c6beea226d05e5f
aaronsleepy <aaron@kmong.com>
2022-11-26 22:10:46 +0900

    feat: GithubProfile Codable 모델 추가

--- Network.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
+++ Network.playground/Pages/Decode Data.xcplaygroundpage/Contents.swift
@@ -1,1 +1,17 @@
 import UIKit
+
+struct GithubProfile: Codable {
+    let login: String
+    let avatarUrl: String
+    let htmlUrl: String
+    let followers: Int
+    let following: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarUrl = "avatr_url"
+        case htmlUrl = "html_url"
+        case followers
+        case following
+    }
+}
